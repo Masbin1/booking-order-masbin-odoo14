03ec784a69b91c946a1ac3132802fd61f7e00b7b
Masbin1 <83834217+Masbin1@users.noreply.github.com>
2022-03-10 09:35:52 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "booking_order_bintang_odoo14",
+
+    'summary': """
+        Booking Order Bintang""",
+
+    'description': """
+        Booking Order Bintang 14
+    """,
+
+    'author': "Mas Bintang",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/wo_sequence.xml',
+        'views/menu_item.xml',
+        'views/sale_order.xml',
+        'views/service_team.xml',
+        'views/work_order.xml',
+        'wizard/cancel_state.xml',
+        'report/work_order_report.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import sales_order
+from . import service_team
+from . import work_order
