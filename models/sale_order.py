96c14cda339dc9d94588b20fc65f72156fa21291
muhammad bintang <masbin@gmail.com>
2022-03-11 10:10:56 +0700

    modify sale order & work order pertama

--- models/sale_order.py
+++ models/sale_order.py
@@ -4,13 +4,28 @@ from odoo.exceptions import Warning
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+    
+    is_booking_order = fields.Boolean(
+        string='Is Booking Order', 
+        default=True)
 
-    is_booking_order = fields.Boolean(string='Is Booking Order', default=True)
-    team = fields.Many2one('service.team', string='Team')
-    team_leader = fields.Many2one('res.users', string='Team Leader')
-    team_members = fields.Many2many('res.users', string='Team Members')
-    booking_start = fields.Datetime(string='Booking Start')
-    booking_end = fields.Datetime(string='Booking End')
+    team = fields.Many2one(
+        'service.team', 
+        string='Team')
+
+    team_leader = fields.Many2one(
+        comodel_name='res.users', 
+        string='Team Leader')
+
+    team_members = fields.Many2many(
+        comodel_name='res.users', 
+        string='Team Members')
+
+    booking_start = fields.Datetime(
+        string='Booking Start')
+        
+    booking_end = fields.Datetime(
+        string='Booking End')
 
     @api.onchange('team')
     def get_team(self):
--- models/work_order.py
+++ models/work_order.py
@@ -4,16 +4,48 @@ class WorkOrder(models.Model):
     _name = 'work.order'
     _description = 'Model Work Order'
 
-    WO_number = fields.Char(string='WO Number', readonly=True, copy=False, default=lambda self: _('New'))
-    booking_order_ref = fields.Many2one('sale.order', string='Booking Order Reference', required=True,)
-    # partner_id = fields.Many2one('res.partner', string='Customer', required=True,)
-    team_id = fields.Many2one('service.team', string='Team', required=True, )
-    team_leader = fields.Many2one('res.users', string='Team Leader', required=True, )
-    team_members = fields.Many2many('res.users', string='Team Members')
-    planned_start = fields.Datetime(string='Planned Start', required=True, )
-    planned_end = fields.Datetime(string='Planned End', required=True, )
-    date_start = fields.Datetime(string='Date Start', readonly=True)
-    date_end = fields.Datetime(string='Date End', readonly=True)
+    WO_number = fields.Char(
+        string='WO Number', 
+        readonly=True, 
+        copy=False, 
+        default=lambda 
+        self: _('New'))
+
+    booking_order_ref = fields.Many2one(
+        'sale.order', 
+        string='Booking Order Reference', 
+        required=True,)
+
+    team_id = fields.Many2one(
+        'service.team', 
+        string='Team', 
+        required=True,) 
+        
+    team_leader = fields.Many2one(
+        'res.users', 
+        string='Team Leader', 
+        required=True,)
+        
+    team_members = fields.Many2many(
+        'res.users', 
+        string='Team Members')
+        
+    planned_start = fields.Datetime(
+        string='Planned Start', 
+        required=True,)
+        
+    planned_end = fields.Datetime(
+        string='Planned End', 
+        required=True,)
+        
+    date_start = fields.Datetime(
+        string='Date Start', 
+        readonly=True,)
+        
+    date_end = fields.Datetime(
+        string='Date End', 
+        readonly=True)
+        
     state = fields.Selection([
             ('pending','Pending'),
             ('pro','In Progress'),
@@ -21,6 +53,7 @@ class WorkOrder(models.Model):
             ('cancel','Cancelled'),], 
             string='State',
             default='pending')
+            
     notes = fields.Text(string='Notes')
 
     @api.model
