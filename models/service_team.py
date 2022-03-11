2ca213c758c31a465b4197833eb54359a33d838b
muhammad bintang <masbin@gmail.com>
2022-03-11 10:12:41 +0700

    add service team

--- models/service_team.py
+++ models/service_team.py
@@ -4,6 +4,15 @@ class ServiceTeam(models.Model):
     _name = 'service.team'
     _description = 'Model Service Team'
 
-    team = fields.Char(string='Team Name', required=True, )
-    team_leader = fields.Many2one('res.users', string='Team Leader', required=True, )
-    team_members = fields.Many2many('res.users',string='Team Member')
+    team = fields.Char(
+        string='Team Name', 
+        required=True, )
+
+    team_leader = fields.Many2one(
+        comodel_name='res.users', 
+        string='Team Leader', 
+        required=True, )
+
+    team_members = fields.Many2many(
+        comodel_name='res.users',
+        string='Team Member')
